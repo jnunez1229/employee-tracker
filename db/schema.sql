3992d4b1f87998d6324b205f0e72c455c4d99cc0
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-03-11 10:11:15 -0600

    created seeds file with values

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,18 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
 
-CREATE TABLE department(
+CREATE TABLE departments(
     id INT PRIMARY KEY
     name VARCHAR(30)
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
     id INT PRIMARY KEY
     title VARCHAR(30)
     salary DECIMAL
     department_id INT
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employee(
@@ -21,6 +21,6 @@ CREATE TABLE employee(
     last_name VARCHAR(30)
     role_id INT
     manager_id INT
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id)
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(name) ON DELETE SET NULL
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id)
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(name) ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO departments (name)
+VALUES
+('Management'),
+('Sales'),
+('Tech'),
+('Human Resources'),
+('Accounting');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Manager', 120000, 1),
+('Sales Representive', 60000, 2),
+('Software Developer', 87000, 3),
+('HR Representative', 80000, 4),
+('Accountant', 72000, 5);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+('Juan', 'Nunez', 1),
+('John', 'Doe', 2),
+('Jim', 'Carey', 5),
+('Ron', 'Burgundy', 3),
+('Adam', 'Sandler', 4),
+('Melissa', 'McCarthy', 3),
+('Sandra', 'Bullock', 2),
+('Gene', 'Wilder', 4),
+('Owen', 'Wilson', 5),
+('Seth', 'McFarlane', 1);
